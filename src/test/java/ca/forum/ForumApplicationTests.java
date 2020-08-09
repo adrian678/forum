694a756f692566970022644fa22487511794d6b3
Adrian-guindani <adrian.guindani@mail.mcgill.ca>
2020-08-09 19:21:23 -0400

    initial commit

---
+++ src/test/java/ca/forum/ForumApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
