f3e2bd3933cbf733a259cd55c98b589b292e2001
Adrian-guindani <adrian.guindani@mail.mcgill.ca>
2020-08-09 21:51:19 -0400

    temporarily removed testing dependencies to check Jenkins build config

--- src/main/java/ca/forum/ForumApplication.java
+++ src/main/java/ca/forum/ForumApplication.java
@@ -1,13 +1,29 @@
 package ca.forum;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class ForumApplication {
+public class ForumApplication implements CommandLineRunner {
+
+	@Autowired
+	private ApplicationContext applicationContext;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForumApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		System.out.println(applicationContext.getDisplayName());
+		System.out.println(applicationContext.getId());
+
+//		MyBean myBean = applicationContext.getBean(MyBean.class);
+//		System.out.println(myBean.getApplicationId());
 	}
 
 }
